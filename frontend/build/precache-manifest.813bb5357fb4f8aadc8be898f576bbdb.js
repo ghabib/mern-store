6ee6d0486f08da323c90c5f31579a7744b0d0d06
Ghulam Habib <ghulamhabib@Ghulams-MBP.attlocal.net>
2020-05-19 03:18:02 -0500

    add project files

---
+++ frontend/build/precache-manifest.813bb5357fb4f8aadc8be898f576bbdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4697b2070308736273e2ee8b961d6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1da7688833b409dea427",
+    "url": "/static/css/main.80a38be0.chunk.css"
+  },
+  {
+    "revision": "5c54e919618332af2d8a",
+    "url": "/static/js/2.8ca357f8.chunk.js"
+  },
+  {
+    "revision": "1da7688833b409dea427",
+    "url": "/static/js/main.3ac99fec.chunk.js"
+  },
+  {
+    "revision": "fa5815e751905378639c",
+    "url": "/static/js/runtime-main.d1e8d6cb.js"
+  }
+]);
